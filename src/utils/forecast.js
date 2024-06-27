fcb9c9e8ba8897f3576f7bf0f3f1b194778b5ca2
Joon Hee Koh <joonheekoh@Mac-mini.modem>
2024-06-27 13:33:30 +1000

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=0e1aeeb614eaf3517e2574d8653a23f7&query=' + latitude + ',' + longitude
+    //  + '&units=f'
+    
+    request({ url, json: true }, (error, { body }) => {
+        if (error)  {
+            callback('Unable to connect to weather services', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else 
+            callback(undefined, body.current.weather_descriptions[0] + ". Its currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.")
+        }
+)}
+
+module.exports = forecast
